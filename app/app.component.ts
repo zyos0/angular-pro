569d757a06bea2c77b327f745c3238fe32caef2c
zyos0 <robertomalaver7@gmail.com>
2018-06-25 22:18:54 -0500

    embedView

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,37 +1,26 @@
-import {
-  AfterContentInit,
-  Component,
-  ComponentFactoryResolver,
-  ComponentRef, TemplateRef,
-  ViewChild,
-  ViewContainerRef
-} from '@angular/core';
-
-import {User} from './auth-form/auth-form.interface';
-import {AuthFormComponent} from "./auth-form/auth-form.component";
+import { Component } from '@angular/core';
+import { AuthFormComponent } from './auth-form/auth-form.component';
+import { User } from './auth-form/auth-form.interface';
 
 @Component({
   selector: 'app-root',
   template: `
     <div>
+      <ng-container
+        [ngTemplateOutlet]="tmpl"
+        [ngTemplateOutletContext]="ctx"
+      >
+      </ng-container>
       <template #tmpl let-name let-location="location">
         {{name}} : {{location}}
       </template>
-      <div #entry></div>
     </div>
   `
 })
-export class AppComponent implements AfterContentInit {
-  component: ComponentRef<AuthFormComponent>;
+export class AppComponent {
 
-  @ViewChild('entry', {read: ViewContainerRef}) entry: ViewContainerRef;
-  @ViewChild('tmpl') tmpl: TemplateRef<any>;
-
-
-  ngAfterContentInit() {
-    this.entry.createEmbeddedView(this.tmpl,{
-      $implicit:"Motto Todd",
-      location:'England, UK'
-    });
+  ctx={
+    $implicit:'Todd Motto',
+    location:"England, UK"
   }
 }
