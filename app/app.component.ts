6cac36eb51117f6169932478cb24c00a2521a572
zyos0 <robertomalaver7@gmail.com>
2018-06-26 10:47:25 -0500

    inmutability onPush

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,26 +1,40 @@
-import { Component } from '@angular/core';
-import { AuthFormComponent } from './auth-form/auth-form.component';
-import { User } from './auth-form/auth-form.interface';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
     <div>
-      <ng-container
-        [ngTemplateOutlet]="tmpl"
-        [ngTemplateOutletContext]="ctx"
-      >
-      </ng-container>
-      <template #tmpl let-name let-location="location">
-        {{name}} : {{location}}
-      </template>
+      <button (click)="addProp()">Add property</button>
+      <button (click)="changeUser()">Change user object</button>
+      <button (click)="changeName()">Change name property</button>
+      <div class="users">
+        <example-one [user]="user"></example-one>
+        <example-two [user]="user"></example-two>
+      </div>
     </div>
   `
 })
 export class AppComponent {
+  user: any = {
+    name: 'Mark Hoppus',
+    age: 44,
+    location: 'California'
+  };
 
-  ctx={
-    $implicit:'Todd Motto',
-    location:"England, UK"
+  addProp() {
+    this.user.email = 'blink@blink-182.net';
+  }
+
+  changeName() {
+    this.user.name = 'Travis Barker';
+  }
+
+  changeUser() {
+    this.user = {
+      name: 'Tom Delonge',
+      age: 41,
+      location: 'California'
+    };
   }
 }
