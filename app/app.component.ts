1fd7f7b6781510bce3a3ddf83d3d7dfcb1b0971f
zyos0 <robertomalaver7@gmail.com>
2018-06-26 13:51:40 -0500

    templateRef

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,40 +1,30 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
     <div>
-      <button (click)="addProp()">Add property</button>
-      <button (click)="changeUser()">Change user object</button>
-      <button (click)="changeName()">Change name property</button>
-      <div class="users">
-        <example-one [user]="user"></example-one>
-        <example-two [user]="user"></example-two>
-      </div>
+      <label>
+        Credit Card Number
+        <input 
+          name="credit-card" 
+          type="text"
+          placeholder="Enter your 16-digit card number"
+          credit-card>
+      </label>
+      <label 
+        tooltip="3 digits, back of your card"
+        #myTooltip="tooltip">
+        Enter your security code 
+        <span
+          (mouseover)="myTooltip.show()"
+          (mouseout)="myTooltip.hide()">
+          (?)
+        </span>
+        <input type="text">
+      </label>
     </div>
   `
 })
 export class AppComponent {
-  user: any = {
-    name: 'Mark Hoppus',
-    age: 44,
-    location: 'California'
-  };
-
-  addProp() {
-    this.user.email = 'blink@blink-182.net';
-  }
-
-  changeName() {
-    this.user.name = 'Travis Barker';
-  }
-
-  changeUser() {
-    this.user = {
-      name: 'Tom Delonge',
-      age: 41,
-      location: 'California'
-    };
-  }
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ExampleOneComponent } from './one/one.component';
-import { ExampleTwoComponent } from './two/two.component';
+import { TooltipDirective } from './tooltip/tooltip.directive';
+import { CreditCardDirective } from './credit-card/credit-card.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExampleOneComponent,
-    ExampleTwoComponent
+    CreditCardDirective,
+    TooltipDirective
   ],
   imports: [
     BrowserModule
