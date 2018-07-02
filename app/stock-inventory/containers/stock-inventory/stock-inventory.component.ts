f333b3edf181b992d29c465c6e66faabc41804ed
zyos0 <robertomalaver7@gmail.com>
2018-07-02 01:09:17 -0500

    formbuilder

--- app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
+++ app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
@@ -20,6 +20,10 @@ import {Observable} from "rxjs/Observable";
                         (remove)="removeStock($event)"
         ></stock-products>
 
+        <div class="stock-inventory__price">
+          Total: {{total | currency: 'USD':true}}
+        </div>
+
 
         <div class="stock-inventory__buttons">
           <button type="submit"
@@ -43,6 +47,7 @@ export class StockInventoryComponent implements OnInit {
 
   products: Product[];
   productMap: Map<number, Product>;
+  total: number;
 
 
   form = this.fb.group({
@@ -66,8 +71,21 @@ export class StockInventoryComponent implements OnInit {
         this.productMap = new Map<number, Product>(myMap);
         this.products = products;
 
-        cart.forEach(item => this.addStock(item))
+        cart.forEach(item => this.addStock(item));
+        this.calculateTotal(this.form.get('stock').value)
+
+        this.form.get('stock')
+          .valueChanges.subscribe(value=>this.calculateTotal(value));
+
+
       })
+  }
+
+  calculateTotal(value: Item[]) {
+    const total = value.reduce((prev, next) => {
+      return prev + next.quantity * this.productMap.get(next.product_id).price;
+    }, 0)
+    this.total = total;
   }
 
   createStock(stock) {
