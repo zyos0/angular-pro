a063cd7eabd67fb4dba8f4f68662aae415259152
zyos0 <robertomalaver7@gmail.com>
2018-07-04 12:09:08 -0500

    router outlet events

--- app/mail/components/mail-app/mail-app.component.ts
+++ app/mail/components/mail-app/mail-app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'mail-app',
   styleUrls: ['mail-app.component.scss'],
   template: `
     <div class="mail">
-      <router-outlet></router-outlet>
+      <router-outlet
+        (activate)="onActivate($event)"
+        (deactivate)="onDeactivate($event)"
+      ></router-outlet>
     </div>
   `
 })
-export class MailAppComponent {}
+export class MailAppComponent {
+
+
+  onActivate(event) {
+    console.log('activate', event);
+  }
+
+  onDeactivate(event) {
+    console.log('deactivate', event)
+  }
+}
