8c6a457fac3620013e91897f29b736f3dbe90141
zyos0 <robertomalaver7@gmail.com>
2018-07-04 11:59:30 -0500

    subscribing to  router events

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,9 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouterModule, Routes} from '@angular/router';
+import {HttpModule} from '@angular/http';
 
-import { StockInventoryModule } from './stock-inventory/stock-inventory.module';
+import {MailModule} from './mail/mail.module';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+
+export const ROUTES: Routes = [
+  {path: '**', redirectTo: 'folder/inbox'}
+];
 
 @NgModule({
   declarations: [
@@ -11,10 +17,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    StockInventoryModule
+    HttpModule,
+    MailModule,
+    RouterModule.forRoot(ROUTES)
   ],
   bootstrap: [
     AppComponent
   ]
 })
-export class AppModule {}
+export class AppModule {
+}
