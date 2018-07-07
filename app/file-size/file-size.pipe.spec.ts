0269ab1651a6662c32af8d1d636bb1ffed3fdb50
zyos0 <robertomalaver7@gmail.com>
2018-07-07 13:53:23 -0500

    shallow Pipe Test

--- app/file-size/file-size.pipe.spec.ts
+++ app/file-size/file-size.pipe.spec.ts
@@ -1,8 +1,62 @@
 import {FileSizePipe} from "./file-size.pipe";
+import {ComponentFixture, TestBed} from "@angular/core/testing";
+import {BrowserDynamicTestingModule, platformBrowserDynamicTesting} from "@angular/platform-browser-dynamic/testing";
+import {Component} from "@angular/core";
+
+TestBed.initTestEnvironment(
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting()
+);
 
 describe('FileSizePipe', () => {
 
-  describe('Isolate FileSizePipe test',()=>{
+  describe('Shallow FileSizePipe test', () => {
+    @Component({
+      template: `
+        Size: {{size | filesize:suffix}}
+      `
+    })
+    class TestComponent {
+      suffix;
+      size = '123456789'
+    }
+
+    let component: TestComponent;
+    let fixture: ComponentFixture<TestComponent>;
+    let el: HTMLElement;
+
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        declarations: [
+          FileSizePipe,
+          TestComponent
+        ]
+      });
+      fixture = TestBed.createComponent(TestComponent);
+      component = fixture.componentInstance;
+      el = fixture.nativeElement;
+    });
+
+
+    it('should convert bytes to megabytes', () => {
+      fixture.detectChanges();
+      expect(el.textContent).toContain('Size: 117.74MB');
+    });
+
+    it('should use the default extension when not supplied', () => {
+      fixture.detectChanges();
+      expect(el.textContent).toContain('Size: 117.74MB');
+    });
+
+    it('should use the default extension when supplied', () => {
+      component.suffix='TNT';
+      fixture.detectChanges();
+      expect(el.textContent).toContain('Size: 117.74TNT');
+    });
+
+  });
+
+  describe('Isolate FileSizePipe test', () => {
     const pipe = new FileSizePipe();
 
     it('should convert bytes to megabytes', () => {
@@ -17,7 +71,7 @@ describe('FileSizePipe', () => {
 
     it('should use the default extension when supplied', () => {
       expect(pipe.transform(123456789, 'myExt')).toBe('117.74myExt');
-      expect(pipe.transform(987654321, 'myExt')).toBe('941.90MB');
+      expect(pipe.transform(987654321, 'myExt')).toBe('941.90myExt');
     })
   })
 
